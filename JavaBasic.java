f55c72f3ce36e11e780809a2d16c32f68620d96d
Subhash <123473222+Subhashdrx2002@users.noreply.github.com>
2024-05-20 13:20:06 +0000

    new

---
+++ JavaBasic.java
@@ -0,0 +1,11 @@
+public class JavaBasic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("1,2,3,4,5");
+    }
+}
+// boilerplate code
