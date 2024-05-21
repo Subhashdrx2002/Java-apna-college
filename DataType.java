0c1e1e5666436bb6f406f5b41a74d5d81666537f
Subhash <123473222+Subhashdrx2002@users.noreply.github.com>
2024-05-21 13:38:54 +0000

    #new

---
+++ DataType.java
@@ -0,0 +1,15 @@
+public class DataType {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = false;
+        System.out.println(var);
+        float price = 10.5f;
+        System.out.println(price);
+        int number = 25;
+        System.out.println(number);
+    }
+    
+}
---
+++ sumvariable.java
@@ -0,0 +1,10 @@
+public class sumvariable {
+     public static void main(String[] args) {
+        // sum of two varibale
+        int a = 10;
+        int b = 5;
+        int sum = a+b;
+        System.out.println(sum);
+     }
+    
+}
