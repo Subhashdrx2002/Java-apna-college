01032af88b1b39b84d9c20122462b18924341deb
Subhash <123473222+Subhashdrx2002@users.noreply.github.com>
2024-05-30 12:07:12 +0000

    #new

---
+++ conditinalstatemenent1.java
@@ -0,0 +1,12 @@
+public class conditinalstatemenent1{
+    public static void main(String[] args) {
+        int age = 16;
+        if(age>=18){
+            System.out.println("adult: derive vote");
+        }
+        else{
+            System.out.println("not adult");
+        }
+        
+    }
+}
---
+++ javaBasic.java
@@ -0,0 +1,13 @@
+public class javaBasic {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        if(a>=b){
+            System.out.println("a is greater then 2");
+        }
+        else{
+            System.out.println("b is larger then 2");
+        }
+    }
+    
+}
